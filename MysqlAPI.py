00de40ff3736d8b7de3ee8471d21d0411139dc8a
shahad <132993961+Shahad-Alhamili@users.noreply.github.com>
2025-02-12 22:28:00 +0300

    Update MysqlAPI.py

--- MysqlAPI.py
+++ MysqlAPI.py
@@ -7,7 +7,7 @@ import mysql.connector
 # Database Connection
 def get_db():
     connection = mysql.connector.connect(
-        host='127.0.0.120',
+        host='127.0.0.1',
         user='root',
         password='pass',
         database='Batoot'
